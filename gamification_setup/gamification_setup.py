3e97053bd58f5a175ba2f046686026586445c2a0
Alexº <lexws33@gmail.com>
2021-09-09 22:18:54 +0200

    automatic update for image build

--- gamification_setup/gamification_setup.py
+++ gamification_setup/gamification_setup.py
@@ -61,15 +61,15 @@ class GamificationSetupXBlock(StudioEditableXBlockMixin, XBlock):
         b = ""
         c = ""
         try:
-            a=str(course.tabs[0].tab_dict.get('url_slug'))
+            b=str(dir(course.tabs))
         except:
             pass
         try:
-            b=str(course.tabs[0].tab_dict.get('link_func', course.tabs.course_reverse_func(self.view_name)))
+            c=str(dir(course.tabs[0]))
         except:
             pass
         try:
-            c=str(course.tabs[0].link_func()(course, course.tabs.course_reverse_func))
+            a=str(course.tabs)
         except:
             pass
 
